4c926823b69a3e74f69b09c3ea76df02051f3023
roynx98 <roynx98@gmail.com>
2025-07-21 11:54:12 -0600

    feat: Handle reduntant downloands

--- server.py
+++ server.py
@@ -3,58 +3,76 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import PlainTextResponse
 import httpx
-import tempfile
-import os
-from markitdown import MarkItDown
-from utils import get_format
-from urllib.parse import urlparse
+from utils import get_format, get_headers, get_markdown
 from datetime import datetime
+import hashlib
 
 app = FastAPI()
-md = MarkItDown()
+eTag = {}
+lastModified = {}
+hashes = {}
 
 class ConvertRequest(BaseModel):
     urls: list[str]
     title: str
+    alwaysGenerate: bool = False
 
 @app.post("/convert-to-md", response_class=PlainTextResponse)
 async def convert_to_md(
     body: ConvertRequest
 ):
-    urls, title = body.urls, body.title
+    urls, title, alwaysGenerate = body.urls, body.title, body.alwaysGenerate
     final_markdown = ""
+    was_modified = True 
+
+    for url in urls:
+        if not was_modified:
+            break
+
+        format = get_format(url)
+        headers = get_headers(url)
+
+        if url in eTag:
+            headers["If-None-Match"] = eTag[url]
+        elif url in lastModified:
+            headers["If-Modified-Since"] = lastModified[url]
+
+        try:
+            async with httpx.AsyncClient(follow_redirects=True) as client:
+                response = await client.get(url, headers=headers)
+
+                if response.status_code == 304:
+                    was_modified = False
+                else:
+                    markdown = get_markdown(response, format)
+                    hash = hashlib.sha256(markdown.encode("utf-8")).hexdigest()
+                    if hashes.get(url, '') == hash:
+                        was_modified = False
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Failed to download file: {e}")
+        
+    if not was_modified and not alwaysGenerate:
+        raise HTTPException(status_code=304, detail="No content modified")
 
     for url in urls:
         format = get_format(url)
-
-        parsed = urlparse(url)
-        referer = f"{parsed.scheme}://{parsed.netloc}/"
-        headers = {
-            "User-Agent": "Mozilla/5.0",
-            "Referer": referer,
-        }
+        headers = get_headers(url)
 
         try:
             async with httpx.AsyncClient(follow_redirects=True) as client:
                 response = await client.get(url, headers=headers)
                 response.raise_for_status()
+                if 'etag' in response.headers:
+                    eTag[url] = response.headers['etag']
+                if 'last-modified' in response.headers:
+                    lastModified[url] = response.headers['last-modified']
         except Exception as e:
             raise HTTPException(status_code=400, detail=f"Failed to download file: {e}")
 
-        if format == "md":
-            final_markdown += "\n" + response.text
-        else:
-            with tempfile.NamedTemporaryFile(delete=False, suffix="." + format) as tmp:
-                tmp.write(response.content)
-                tmp_path = tmp.name
+        markdown = get_markdown(response, format)
 
-            try:
-                result = md.convert(tmp_path)
-            except Exception as e:
-                os.unlink(tmp_path)
-                raise HTTPException(status_code=500, detail=f"Conversion failed: {e}")
-            os.unlink(tmp_path)
-            final_markdown += result.text_content
+        hashes[url] = hashlib.sha256(markdown.encode("utf-8")).hexdigest()
+        final_markdown += markdown + "\n"
 
     date_downloaded = datetime.now().strftime("%Y-%m-%d")
     meta = f"""
--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+from fastapi import HTTPException
+from urllib.parse import urlparse
+import tempfile
+from markitdown import MarkItDown
+import os
+
+md = MarkItDown()
+
 def get_format(url):
     if "docx" in url:
         return 'docx'
@@ -5,3 +13,30 @@ def get_format(url):
         return 'doc'
 
     return 'md'
+
+def get_headers(url: str) -> dict:
+    parsed = urlparse(url)
+    referer = f"{parsed.scheme}://{parsed.netloc}/"
+    return {
+        "User-Agent": "Mozilla/5.0",
+        "Referer": referer,
+    }
+
+def get_markdown(response, format) -> str:
+    mardown = ""
+    if format == "md":
+        mardown = response.text
+    else:
+        with tempfile.NamedTemporaryFile(delete=False, suffix="." + format) as tmp:
+            tmp.write(response.content)
+            tmp_path = tmp.name
+
+            try:
+                result = md.convert(tmp_path)
+            except Exception as e:
+                os.unlink(tmp_path)
+                raise HTTPException(status_code=500, detail=f"Conversion failed: {e}")
+            os.unlink(tmp_path)
+            
+            mardown = result.text_content
+    return mardown
