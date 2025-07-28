35ddf42f1857a9c853d8978649097ab68d4f55e4
roynx98 <roynx98@gmail.com>
2025-07-28 13:02:11 -0600

    feat(utils): Add more request headers

--- utils.py
+++ utils.py
@@ -24,8 +24,10 @@ def get_headers(url: str) -> dict:
     parsed = urlparse(url)
     referer = f"{parsed.scheme}://{parsed.netloc}/"
     return {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
         "Referer": referer,
+        "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8a',
+        "Accept-Language": 'en',
     }
 
 def get_markdown(response, format, css_selector) -> str:
